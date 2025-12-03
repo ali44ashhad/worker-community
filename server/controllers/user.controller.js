092e26ced12093d760190350f164a2da66d59065
Dishant Sharma <dishanthirakv@gmail.com>
2025-12-03 11:14:23 +0530

    Increase the cookie TTL

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,7 +17,7 @@ const generateToken = (userId, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         path: "/",
     });
 };
